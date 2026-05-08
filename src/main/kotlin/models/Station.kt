6929a152a025c6b07951e9bacc4abab48b9db301
keg-git <keg.git@proton.me>
2026-05-08 17:27:36 -0400

    transferring to laptop

--- src/main/kotlin/models/Station.kt
+++ src/main/kotlin/models/Station.kt
@@ -1,3 +1,9 @@
 package keggit.shotlin.models
 
-class Station(val courseId: int)
+class Station(val courseId: int) {
+
+    var launcher1Desc String
+    var launcher2Desc String
+    var editDate String
+
+}
